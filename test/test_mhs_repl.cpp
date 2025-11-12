4392f61fb63fd5bdb5b3688453638da24566b4b0
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-11-12 18:56:06 +0900

    Add trim

--- test/test_mhs_repl.cpp
+++ test/test_mhs_repl.cpp
@@ -1,25 +1,47 @@
 #include <boost/ut.hpp>
 #include <xeus-haskell/mhs_repl.hpp>
+#include <string>
+#include <string_view>
+#include <algorithm>
+#include <cctype>
+#include <ranges>
+
 using namespace boost::ut;
 using namespace std::string_literals;
 
-int main() {
+auto trim(std::string_view s) -> std::string {
+    auto is_space = [](unsigned char c) { return std::isspace(c); };
 
-  "positive repl test"_test = [] {
-    xeus_haskell::MicroHsRepl repl;
-    auto result = repl.execute("1 + 1");
-    expect(eq(result.value(), "2\n"s));
-  };
+    auto trimmed = s 
+        | std::views::drop_while(is_space)
+        | std::views::reverse
+        | std::views::drop_while(is_space)
+        | std::views::reverse;
 
-
-  "basic test"_test = [] {
-    expect(42_i == 42);
-    expect(1_i != 2);
-  };
-
-  "sum test"_test = [] {
-    constexpr auto sum = [](auto... values) { return (values + ...); };
-    expect(sum(1, 2) == 3_i);
-    expect(sum(0) == 0_i);
-  };
+    return {trimmed.begin(), trimmed.end()};
 }
+
+auto expect_trim_eq = [](std::string_view a, std::string_view b) {
+    expect(eq(trim(a), trim(b))) << "expected (ignoring spaces): " << a << " == " << b;
+};
+
+int main() {
+    "positive repl test"_test = [] {
+        xeus_haskell::MicroHsRepl repl;
+        auto result = repl.execute("1 + 1");
+
+        expect_trim_eq(result.value(), "2");
+    };
+
+    "basic test"_test = [] {
+        expect(42_i == 42);
+        expect(1_i != 2);
+    };
+
+    "sum test"_test = [] {
+        constexpr auto sum = [](auto... values) { return (values + ...); };
+        expect(sum(1, 2) == 3_i);
+        expect(sum(0) == 0_i);
+    };
+}
+
