d2643dd8d74d290b56ac82781e95e7ba59dccc58
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-11-15 09:49:53 +0000

    wasm: link post.js and add jupyterlite deps

--- src/post.js
+++ src/post.js
@@ -1,11 +1,1 @@
-Module['FS'] = FS;
-Module['PATH'] = PATH;
-Module['LDSO'] = LDSO;
 Module['getDylinkMetadata'] = getDylinkMetadata;
-Module['loadDynamicLibrary'] = loadDynamicLibrary;
-
-Module.FS = FS;
-
-if (!('wasmTable' in Module)) {
-    Module['wasmTable'] = wasmTable
-}
